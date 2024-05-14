3820bd79e70591668817031cd65001d68d599a6b
IsraelMobote <moboteisrael02@gmail.com>
2024-05-14 04:00:58 +0100

    edit

--- scripts/debugging.js
+++ scripts/debugging.js
@@ -1,15 +1,14 @@
-let d = 7;
-const output = document.querySelector("ul");
-const options = { weekday: 'long' };
-const today = new Date();
-let todayString = new Intl.DateTimeFormat('en-US', options).format(today);
-document.querySelector("p").innerHTML = `today : ${todayString}`;
 
-for (let i = 1; i < d; i++) {
-    let nextDay = new Date();
-    nextDay.setDate(today.getDate() + i);
-    let nextDayString = new Intl.DateTimeFormat('en-US', options).format(nextDay);
-    let lid = document.createElement('li');
-    lid.textContent = nextDayString;
-    output.appendChild(lid);
+
+//function fullName(first, last) {
+//    const firstName = first;
+//const lastName = last;
+ //   return `${firstName} ${lastName}`;
+//
+
+
+
+const fullName = function (first,last) {
+    return `${first} ${last}`;
 }
+document.querySelector('#fullName').textContent = fullName('Israel','Mobote');
