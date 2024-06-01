143fd2935fffb8300b4de44e84235cde7014a452
IsraelMobote <moboteisrael02@gmail.com>
2024-06-01 23:55:46 +0100

    edit project

---
+++ project/scripts/project.js
@@ -0,0 +1,10 @@
+
+const menu = document.querySelector("#menu");
+const navi = document.querySelector("nav")
+
+menu.addEventListener('click', func)
+
+function func() {
+    menu.classList.toggle("show");
+    navi.classList.toggle("show")
+}
